eda8ac9d811245e4f5b4d2bfbc19db138a56b8ee
Артур Захаркин <zaharkin.artur75@gmail.com>
2024-04-02 19:40:35 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Hospital
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.btSimulate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btStop = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btSimulate
+            // 
+            this.btSimulate.Location = new System.Drawing.Point(768, 11);
+            this.btSimulate.Margin = new System.Windows.Forms.Padding(2);
+            this.btSimulate.Name = "btSimulate";
+            this.btSimulate.Size = new System.Drawing.Size(93, 50);
+            this.btSimulate.TabIndex = 0;
+            this.btSimulate.Text = "Simulate";
+            this.btSimulate.UseVisualStyleBackColor = true;
+            this.btSimulate.Click += new System.EventHandler(this.btSimulate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Продолжительность обследования одного пациента";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(71, 42);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(88, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btStop);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btSimulate);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(-52, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(880, 134);
+            this.panel1.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(349, 95);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(88, 20);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(349, 41);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(88, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(346, 12);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(226, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Скорость поступления пациентов чел/мес.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(346, 65);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(190, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Скорость ухода пациентов чел/мес.\r\n";
+            // 
+            // btStop
+            // 
+            this.btStop.Location = new System.Drawing.Point(768, 65);
+            this.btStop.Margin = new System.Windows.Forms.Padding(2);
+            this.btStop.Name = "btStop";
+            this.btStop.Size = new System.Drawing.Size(93, 50);
+            this.btStop.TabIndex = 7;
+            this.btStop.Text = "Stop";
+            this.btStop.UseVisualStyleBackColor = true;
+            this.btStop.Click += new System.EventHandler(this.btStop_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(-9, 121);
+            this.chart1.Margin = new System.Windows.Forms.Padding(2);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Скорость поступления пациентов";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Скорость ухода пациентов";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(837, 326);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "chart1";
+            // 
+            // Timer
+            // 
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 431);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btSimulate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.Button btStop;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Hospital
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random random = new Random();
+
+        int month;
+        
+
+        int patient_admission_rate, //скорость поступления пациентов
+            patient_reduction_speed,//скорость ухода пациентов
+            patient_num = 20,//кол-во больных
+            hospital_workload = 50,//загруженность больницы
+            hospital_expansion = 1,//расширение больницы
+            num_of_employees= 30,//кол-во персонала
+            duration_of_treatment = 7,//продолжительность лечения
+            duration_of_the_examination,//продолжительность обследования
+            new_requests = 2; //новые обращения
+
+        double patient_admission_rate_coef = 0.5 , //скорость поступления пациентов
+             patient_reduction_speed_coef = 0.5,//скорость ухода пациентов
+             patient_num_coef = 1,//кол-во больных
+             hospital_workload_coef = 0.8,//загруженность больницы
+             hospital_expansion_coef = 0.7,//расширение больницы
+             num_of_employees_coef = 0.8,//кол-во персонала
+             duration_of_treatment_coef = 0.7,//продолжительность лечения
+             duration_of_the_examination_coef = 0.6,//продолжительность обследования
+             new_requests_coef = 0.6; //новые обращения  
+
+
+        private void btStop_Click(object sender, EventArgs e)
+        {
+            Timer.Stop();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+
+            month++;
+
+
+
+            patient_num += (int)(patient_admission_rate_coef * patient_admission_rate);
+           
+            hospital_expansion = random.Next(0, 100);
+            num_of_employees += (int)(hospital_expansion_coef*hospital_expansion);
+            hospital_workload += (int)(-num_of_employees_coef * num_of_employees - hospital_expansion_coef * hospital_expansion + patient_num_coef * patient_num);
+
+            duration_of_treatment += (int)(hospital_workload_coef * hospital_workload + duration_of_the_examination_coef * duration_of_the_examination);
+
+            patient_reduction_speed += (int)(-duration_of_treatment_coef * duration_of_treatment );
+
+            new_requests += (int)(-duration_of_treatment_coef * duration_of_treatment);
+
+            patient_admission_rate += (int)(new_requests_coef * new_requests);
+           
+            
+            chart1.Series[0].Points.AddXY(month, patient_admission_rate);
+            chart1.Series[1].Points.AddXY(month, patient_reduction_speed);
+
+        }
+
+        private void btSimulate_Click(object sender, EventArgs e)
+        {
+            month = 0;
+
+            duration_of_the_examination = Convert.ToInt32(textBox1.Text);
+            patient_admission_rate = Convert.ToInt32(textBox2.Text); 
+            patient_reduction_speed = Convert.ToInt32(textBox3.Text);
+
+            //custom = random.Next(10000, 40000); //кол-во заказов
+            //industry = random.Next(50000, 100000); //кол-во груза
+
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();          
+           
+            chart1.Series[0].Points.AddXY(month, patient_admission_rate);
+            chart1.Series[1].Points.AddXY(month, patient_reduction_speed);
+
+            if (!Timer.Enabled)
+            {
+                Timer.Start();
+            }
+        }
+    }
+}
